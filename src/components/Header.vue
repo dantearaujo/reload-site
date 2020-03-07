07ebd88ef3ff82488e4d6509bd3a59e4cb691e4b
dantearaujo <dante.araujo@gmail.com>
2020-03-06 22:05:33 -0300

    Initial Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="header">
+        <header class="header">
+            <div class="logo">
+                <img src="https://agenciametodo.com/wp-content/uploads/2017/05/logometodoazul.png" alt="Logo">
+            </div>
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><router-link to="/">Home</router-link></li>
+                        <li><router-link to="/sobre">Sobre</router-link></li>
+                        <li><router-link to="/contato">Contato</router-link></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+header {
+    width: 100vw;
+    height: 80px;
+    background-color:#dadada;
+    position: absolute;
+
+}
+
+.logo {
+
+}
+
+.menu {
+
+}
+</style>
